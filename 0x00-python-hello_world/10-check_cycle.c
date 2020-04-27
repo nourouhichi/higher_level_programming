b2ca99f389d20e247cfb36a8756bed370aa5abad
nourouhichi <1417@holbertonschool.com>
2020-04-27 21:29:51 +0100

    first try

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include "stdio.h"
+/**
+ *check_cycle - checks if the linked list has a cycle in it
+ *@list: the linked list to be checked
+ *Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first, *roadrunner;
+
+	if (!list)
+		return (0);
+	first = list;
+	roadrunner = list->next;
+	while (first)
+	{
+		while (roadrunner)
+		{
+			if (roadrunner == first)
+				return (1);
+			roadrunner = roadrunner->next;
+
+		}
+		first = first->next;
+	}
+
+	return (0);
+}
+
