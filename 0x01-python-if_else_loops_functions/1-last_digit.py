f44073e4c86be5bc42b93348433e0666272295fb
nourouhichi <1417@holbertonschool.com>
2020-04-28 14:37:25 +0100

    i dodn't know what is the problem

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,9 @@ else:
     last = number % 10
 string = "last digit of {} is {} and is {}"
 if last > 5:
-    print(string .format(n, last, 'greater than 5'))
-elif last < 6 and last != 0:
-    print(string .format(n, last, 'less than 6 and not 0'))
+    print(string .format(n, last, "greater than 5"))
+elif last == 0:
+    print(string .format(n, last, "0"))
 else:
-    print(string .format(n, last, '0'))
+    print(string .format(n, last, "less than 6 and not 0"))
+
