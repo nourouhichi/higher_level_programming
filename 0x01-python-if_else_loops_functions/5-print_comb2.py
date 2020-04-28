a891ea9777b67578d9caa2753f5f1bb86335fc3f
nourouhichi <1417@holbertonschool.com>
2020-04-28 15:19:58 +0100

    aman ekhdem

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    if x < 10:
-        print("0{}, ".format(x), end="")
-    else:
-        print("{}, ".format(x), end="")
+for x in range(99):
+    print("{0:02d}, ".format(x), end="")
 print("99")
