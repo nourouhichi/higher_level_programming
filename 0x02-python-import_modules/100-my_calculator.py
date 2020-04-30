2eb867e46a729de2447c4b02b7d130cabba13a0e
nourouhichi <1417@holbertonschool.com>
2020-04-30 14:53:25 +0100

    try 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,13 +8,13 @@ if __name__ == "__main__":
     a = int(argv[1])
     b = int(argv[3])
     if argv[2] is "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif argv[2] is "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif argv[2] is "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif argv[2] is "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
