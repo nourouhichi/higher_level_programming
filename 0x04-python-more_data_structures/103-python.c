6fc154f4619beeaf81e4978a90309c85e2bd8193
nourouhichi <1417@holbertonschool.com>
2020-05-07 23:27:16 +0100

    try 2

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,12 +1,33 @@
 #include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_python_list - prints some info
+ * @p: python object
+ */
 void print_python_list(PyObject *p)
 {
+	Py_ssize_t i, j = 0;
 	PyObject *list;
 
-	printf();
+	i = (((PyVarObject *)(p))->ob_size);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %lu\n", i);
+	printf("[*] Allocated = %lu\n", (((PyListObject *)(p))->allocated));
+
+	while (j < i)
+	{
+		list = (((PyListObject *)(p))->ob_item[j]);
+		printf("Element %lu: %s\n", j, (char *)list->ob_type->tp_name);
+		j++;
+
+	}
 }
+/**
+ *print_python_bytes - prints bytes
+ *@p: python object
+ */
 void print_python_bytes(PyObject *p)
 {
-	printf()
+	(void) p;
 }
