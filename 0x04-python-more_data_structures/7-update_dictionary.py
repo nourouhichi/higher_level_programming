754ddcc7a153cdb127659e9ddbeb59caa95472d1
nourouhichi <1417@holbertonschool.com>
2020-05-07 15:06:10 +0100

    try 2

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,6 @@ def update_dictionary(a_dictionary, key, value):
         if key is i:
             a_dictionary[i] = value
             return a_dictionary
-        else:
-            a_dictionary[key] = value
-            return a_dictionary
+    else:
+        a_dictionary[key] = value
+        return a_dictionary
