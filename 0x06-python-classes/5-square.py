a3ba8099d39d8f9a39943666467f8a8a9ba93587
nourouhichi <1417@holbertonschool.com>
2020-05-19 22:59:01 +0100

    new add

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size is 0:
+            print()
+
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
