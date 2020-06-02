af1eee35ff0102872d616329e4df285aea3d8154
nourouhichi <1417@holbertonschool.com>
2020-06-02 22:22:30 +0100

    new try

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
+"""
+module 1 
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-module 1
-"""
 
 
 class Square(Rectangle):
     """ class 1"""
     def __init__(self, size):
+        """function 1"""
         size = super().integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        """function 2"""
         return self.__size * self.__size
 
     def __str__(self):
+        """function 3"""
         stri = "[Rectangle] "
         stri += str(self.__size) + '/' + str(self.__size)
         return stri
