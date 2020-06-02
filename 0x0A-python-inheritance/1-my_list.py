ddd87c58ac4d48a33aa67511b2bd43795549fdae
nourouhichi <1417@holbertonschool.com>
2020-06-02 21:25:00 +0100

    new try

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,5 @@ module 1
 class MyList(list):
     """class 1 """
     def print_sorted(self):
+        """function 1"""
         print(sorted(self))
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,9 +5,11 @@
 class BaseGeometry:
     """class 1"""
     def area(self):
+        """function 1"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
+        """function 2"""
         if type(value) is not int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
