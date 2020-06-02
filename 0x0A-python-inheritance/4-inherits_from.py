519e92cfd7db0151137abdff555e3cdb2182ae83
nourouhichi <1417@holbertonschool.com>
2020-06-02 20:03:10 +0100

    new try

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+module 1
+"""
+
+
 def inherits_from(obj, a_class):
+    """ class 1"""
     if issubclass(type(obj), a_class):
         if type(obj) is not a_class:
             return True
