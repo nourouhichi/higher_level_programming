cbb0fa2a1877ed70354c77e81e62bfe3faba9f9a
nourouhichi <1417@holbertonschool.com>
2020-06-02 22:52:02 +0100

    new add

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,3 +9,8 @@ class MyList(list):
     def print_sorted(self):
         """function 1"""
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
