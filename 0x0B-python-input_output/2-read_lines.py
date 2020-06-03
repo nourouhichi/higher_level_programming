67fbe1609f4053b787b9fd8ca17de45a2ac6e0de
nourouhichi <1417@holbertonschool.com>
2020-06-03 17:16:55 +0100

    new try

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,15 +7,16 @@ module 1
 def read_lines(filename="", nb_lines=0):
     """function 1"""
     counter = 0
-    lines = ""
+    lines =""
     with open(filename, 'r') as file:
         if nb_lines == 0:
             reading = file.read()
             print(reading, end="")
             return
         for line in file:
+            lines += str(line)
+            counter += 1
             if counter == nb_lines:
                 print(lines, end="")
                 return
-            lines += str(line)
-            counter += 1
+
