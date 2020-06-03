26e0334fdf637bd514693ca8654de9fcbc818cbe
nourouhichi <1417@holbertonschool.com>
2020-06-03 18:03:10 +0100

    new add

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module 1
+"""
+
+
+def load_from_json_file(filename):
+    """function1"""
+    import json
+    with open(filename, 'r') as file:
+        return jason.loads(file)
