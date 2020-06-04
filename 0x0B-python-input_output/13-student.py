69d686e994020370555928a538d675309ea47d44
nourouhichi <1417@holbertonschool.com>
2020-06-04 09:42:06 +0100

    new add

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+mudule 1
+"""
+
+
+class Student:
+    """class 1"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        dic = {}
+        for a in attrs:
+            if a in self.__dict__:
+                dic[a] = self.__dict__[a]
+        return dic
+
+    def reload_from_json(self, json):
+        for key, val in json.items():
+            self.__dict__[key] = val
