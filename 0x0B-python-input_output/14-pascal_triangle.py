a31720314c58851989c5343bbee6df3b55e6fa53
nourouhichi <1417@holbertonschool.com>
2020-06-04 10:07:02 +0100

    new try

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -5,14 +5,15 @@ module 1
 
 
 def pascal_triangle(n):
+    """function 1"""
     listy = []
     if n <= 0:
         return listy
     for i in range(n):
         listy.append([])
-        if i != 0:
-            listy[i].append(1)
+        listy[i].append(1)
         for j in range(1, i):
             listy[i].append(listy[i-1][j-1] + listy[i-1][j])
-        listy[i].append(1)
+        if i != 0:
+            listy[i].append(1)
     return listy
