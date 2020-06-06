e4f30aa7af30dbc1be49f9fe6949bf93d7630b18
nourouhichi <1417@holbertonschool.com>
2020-06-06 17:45:26 +0100

    new try

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+module 1
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """function 1"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
