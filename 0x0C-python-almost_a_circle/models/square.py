7c43ce2587bea36bad47266b316f792715d9028f
nourouhichi <1417@holbertonschool.com>
2020-06-09 21:57:48 +0100

    new try

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,21 +8,27 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """class 1"""
     def __init__(self, size, x=0, y=0, id=None):
+        """function 1"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        """function 2"""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
+        """function 3"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """function 4"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """function 5"""
         if args:
             for i, j in enumerate(args):
                 if i is 0:
@@ -39,4 +45,5 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
+        """function 6"""
         return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
