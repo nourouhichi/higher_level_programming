ed0195ba922eb1fbdb3dc01f2b4c3ce60d1f5f2c
nourouhichi <1417@holbertonschool.com>
2020-06-09 21:52:34 +0100

    new try

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """function 11"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format
-    (self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """function 12"""
