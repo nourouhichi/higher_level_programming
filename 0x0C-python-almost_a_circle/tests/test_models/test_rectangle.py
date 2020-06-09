5bada62248ee8a233924d09f7371009ba7eba771
nourouhichi <1417@holbertonschool.com>
2020-06-09 22:23:00 +0100

    new try

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """
 tests for class rectangle
 """
@@ -10,13 +9,13 @@ class RectangleTest(unittest.TestCase):
     """ class 1"""
 
     def test(self):
+        """function 1"""
         rect = Rectangle(5, 2)
         self.assertEqual(rect.width, 5)
         self.assertEqual(rect.height, 2)
         rect = Rectangle(1, 2, 3, 4)
         self.assertEqual(rect.x, 3)
         self.assertEqual(rect.y, 4)
-        self.assertEqual(rect.area, 10)
 
         """ testing on width"""
         self.assertRaises(TypeError, Rectangle, 1.25, 2)
