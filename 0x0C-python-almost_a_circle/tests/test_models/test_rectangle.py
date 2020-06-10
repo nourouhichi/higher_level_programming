838bfc7265adc6248f7efa7329b9417d617e0e6f
nourouhichi <1417@holbertonschool.com>
2020-06-10 10:28:12 +0100

    fixing

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,23 +5,29 @@ import unittest
 from models.rectangle import Rectangle
 
 
-class RectangleTest(unittest.TestCase):
+class TestRectangle(unittest.TestCase):
     """ class 1"""
 
-    def test(self):
+    def test_rectangle_class(self):
         """function 1"""
         rect = Rectangle(5, 2)
         self.assertEqual(rect.width, 5)
         self.assertEqual(rect.height, 2)
+
+    def test_instance_of_rectangle(self):
+        """test instance rectange"""
         rect = Rectangle(1, 2, 3, 4)
         self.assertEqual(rect.x, 3)
         self.assertEqual(rect.y, 4)
 
+    def test_on_width_attribute(self):
         """ testing on width"""
         self.assertRaises(TypeError, Rectangle, 1.25, 2)
         self.assertRaises(TypeError, Rectangle, "1.25", 2)
         self.assertRaises(ValueError, Rectangle, -3, 2)
         self.assertRaises(ValueError, Rectangle, 0, 2)
+
+    def test_on_height_attributes(self):
         """ testing on height"""
         self.assertRaises(TypeError, Rectangle, 2, 5.25)
         self.assertRaises(TypeError, Rectangle, 2, "5.25")
@@ -32,5 +38,5 @@ class RectangleTest(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, 2, 2, 1, 1.25)
         self.assertRaises(ValueError, Rectangle, 2, 5, 1, -5)
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
