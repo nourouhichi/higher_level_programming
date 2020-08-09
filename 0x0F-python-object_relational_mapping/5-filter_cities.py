9ef21c667d4529838dd3064d11a9075847e60bd5
nourouhichi <1417@holbertonschool.com>
2020-08-09 23:32:11 +0100

    ekher marr awlh

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,17 +6,14 @@ A script that lists all states from the database hbtn_0e_0_usa
 
 import MySQLdb
 from sys import argv
-if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=username,
-                         passwd=password, db=db_name, port=3306)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("""SELECT cities.name FROM cities WHERE state_id = (SELECT
                 id FROM states WHERE name='%s')""" % argv[4])
-    rows = cur.fetchall()
-        print(", ".join(row[0] for row in rows)
+    query_rows = cur.fetchall()
+    print(", ".join(row[0] for row in query_rows))
     cur.close()
     db.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -14,6 +14,6 @@ class City(Base):
     """ creating the state table via python"""
     __tablename__ = 'cities'
 
-    id = Column(Integer, autoincrement=True, primary_key=True,nullable=False)
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
