518961c85da2bc75eab4236f0fe608030ad9f41f
nourouhichi <1417@holbertonschool.com>
2020-08-09 22:14:12 +0100

    new add

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that lists all states from the database hbtn_0e_0_usa
+"""
+
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+    conn = engine.connect()
+    dele = State.delete().where(State.name.like('%a%'))
+    conn.execute(dele)
